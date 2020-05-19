ed2269f657cac2c48bab188b881d52558886d740
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-05-19 13:08:27 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,87 @@
+// let breeds = []
 
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
 
-
-
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
-document.addEventListener("DOMContentLoaded", function(e){
-    fetch(imgUrl)
-    .then(resp => resp.json())
-    .then(json =>  { json.message.forEach ( img => addImages(img))})
-    let breeds =  []
-    fetch(breedUrl)
-    .then(resp => resp.json())
-    .then(json =>  {breeds = Object.keys(json.message)
-    addBreeds(breeds)})
-    const select =  document.getElementById("breed-dropdown")
-    select.addEventListener("change", function(e){
-        let letter = e.target.value 
-       let newBreeds = breedFilter(breeds, letter)
-       addBreeds(newBreeds)
-    })
-
+    fetchImg()
+    fetchBreeds()
+    
 })
 
-function addBreeds(breeds){
-const ul =  document.getElementById("dog-breeds")
-ul.innerText = ""
-breeds.forEach(breed => makeli(breed))
-}
-
-function makeli(breed){
-    const li =  document.createElement("li")
-    li.innerText = breed  
-    const ul =  document.getElementById("dog-breeds")
-    ul.appendChild(li)
-    li.addEventListener("click", function(e){
-        e.target.style.color = 'green'
+function fetchImg(){
+    const myFetch = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(myFetch)
+    .then(res => res.json())
+    .then(res => {
+        res.message.forEach(image => addImg(image))
     })
 }
 
-function addImages(img_url){
-    const image =  document.createElement("img")
-    image.src = img_url 
-    const div =  document.getElementById("dog-image-container")
-    div.appendChild(image)
-
-
+function addImg(ozzies){
+    const getId = document.getElementById("dog-image-container")
+    const createId = document.createElement("img")
+    createId.src = ozzies
+    getId.appendChild(createId)
 }
 
-function breedFilter(breeds, letter){
-   return breeds.filter(breed => breed[0] == letter)
+function fetchBreeds(){
+    //fetch todas as breeds
+     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+     fetch(breedUrl)
+     //converte a informacao a json(so humans can read)
+     .then(res => res.json())
+     .then(json => {
+        //pega essa info em formato json e coloca dentro de um array Object.keys nome = breeds
+        //associa o json a message(nome do objeto que queremos manipular/mostrar atraves de dot notation)
+          breeds = Object.keys(json.message)
+          //essa funcao "pega" o node/html
+          //e depois para cara item/element(forEach (output each element inside array)) 
+          //lembrando que a este ponte o json foi convertido a um array(Object.keys) 
+                 updateBreedList(breeds)
+                 addBreedSelectListener();
+    })
+}
+
+function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    //addBreed esta pegando o elemento/node no DOM--cria um novo li
+    //associa o novo li innerText a breed(parametro)
+    //depois append o novo li ao node do html
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
     
+  }
+
+function addBreed(breed){
+    //comentarios acima
+    const getBreedId = document.getElementById("dog-breeds")
+    const createLi = document.createElement('li')
+    createLi.innerText = breed
+    createLi.style.cursor = 'pointer';
+    getBreedId.appendChild(createLi)
+    createLi.addEventListener('click', changeColor)
 }
 
+function changeColor(word){
+    //target se refere ao array(que mudara a cor ao click que foi referido acima pelo pointer)
+ word.target.style.color = 'palevioletred'
+}
+
+function removeChildren(element) {
+    //o que eh lastelement?
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
